bdc9f238b5cd7ddd6959115b52443b7752b70a53
Daniel Stahl <danstahl1138@gmail.com>
2020-09-07 07:05:55 -0500

    fixed comparison

--- src/lib/auth.rs
+++ src/lib/auth.rs
@@ -1,3 +1,4 @@
+use hex_literal::hex;
 use rocket::http::Status;
 use rocket::request::{self, FromRequest, Request};
 use rocket::Outcome;
@@ -8,9 +9,7 @@ pub struct ApiKey(String);
 /// Returns true if `key` is a valid API key string.
 fn is_valid(key: &str) -> bool {
     let result = Sha256::new().chain(key).finalize();
-    println!("{}", str::from_utf8(result.as_slice()).unwrap());
-    result.as_slice()
-        == "3cfb19bbd129ebbaa28ea4368d7fe4329e193c4a5d108002b771104e35fcf172".as_bytes()
+    result.as_slice() == hex!("3cfb19bbd129ebbaa28ea4368d7fe4329e193c4a5d108002b771104e35fcf172")
 }
 
 #[derive(Debug)]
@@ -29,7 +28,7 @@ impl<'a, 'r> FromRequest<'a, 'r> for ApiKey {
         match keys.len() {
             0 => Outcome::Failure((Status::BadRequest, ApiKeyError::Missing)),
             1 if is_valid(keys[0]) => Outcome::Success(ApiKey(keys[0].to_string())),
-            1 => Outcome::Failure((Status::BadRequest, ApiKeyError::Invalid)),
+            1 => Outcome::Failure((Status::Forbidden, ApiKeyError::Invalid)),
             _ => Outcome::Failure((Status::BadRequest, ApiKeyError::BadCount)),
         }
     }
