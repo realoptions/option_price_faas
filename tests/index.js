60f7305fdba93ba9cf618a053930dbff0bbbf9e8
Daniel Stahl <danstahl1138@gmail.com>
2020-11-27 06:55:15 -0600

    improved example prices and added node executors

---
+++ tests/index.js
@@ -0,0 +1,27 @@
+'use strict'
+const fetch = require('node-fetch')
+const { location } = require('./binaryLocation.json')
+const body = require('./example_calibration_2.json')
+const { spawn } = require('child_process')
+const port = '8090'
+const version = 'v2'
+
+const server = spawn(location, [], { env: { PORT: port, MAJOR_VERSION: version } })
+const get_price = (port, model) => {
+    return fetch(
+        `http://localhost:${port}/v2/${model}/calibrator/call`,
+        { method: 'POST', body: JSON.stringify(body), headers: { 'Content-Type': 'application/json' }, }
+    ).then(res => res.json()).then(response => {
+        console.log("This is model " + model)
+        console.log(response.parameters)
+    })
+}
+setTimeout(() => {
+    const models = ["heston", "merton", "cgmy"]
+    Promise.all(models.map(model => get_price(port, model))).finally(() => server.kill())
+}, 1000)//wait for server to launch
+
+
+
+
+
