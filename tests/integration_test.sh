2dcb7dd0e9dee807bc3a91d8a0349700bb59ee4d
Daniel Stahl <danstahl1138@gmail.com>
2021-08-22 19:52:14 -0500

    updated integration tests

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -2,5 +2,5 @@ sed -i 's/${VERSION_MAJOR}/v2/' ./docs/openapi_gcp.yml
 sed -i 's/${HOST}/localhost/' ./docs/openapi_gcp.yml
 sed -i 's/${PROJECT_ID}/123/' ./docs/openapi_gcp.yml
 sed -i 's/${VISIBLE_HOST}/localhost/' ./docs/openapi_gcp.yml
-PORT=9000 MAJOR_VERSION=v2 nohup  ./target/release/option_price &
+ROCKET_PORT=9000 ROCKET_ADDRESS=0.0.0.0 MAJOR_VERSION=v2 nohup  ./target/release/option_price &
 npx dredd ./docs/openapi_gcp.yml http://localhost:9000
